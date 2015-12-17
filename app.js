e6e2742930515cbc20620e99866165deaa767076
ThaiSmartDev <dev@thaismartdev.com>
2015-12-17 14:18:44 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var http = require('http')
+	, express = require('express')
+	, favicon = require('serve-favicon')
+	, fs = require('fs')
+	, path = require('path')
+	, methodOverride = require('method-override')
+	, bodyParser = require('body-parser')
+	, errorHandler = require('errorhandler')
+	, routes = require('./routes');
+
+global.config = require('./config.js');
+
+var app = express();
+
+app.set('port', config.port || 9999);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if ('development' == app.get('env')) {
+	app.use(errorHandler());
+}
+
+app.get('*', function(req, res) {
+	//## Initial Data ##//
+	data = {};
+	data.screen = 'index';
+	data.shop = config.shop;
+	data.apiUrl = config.apiUrl;
+	data.apiKey = config.apiKey;
+	data.websiteUrl = config.websiteUrl;
+	data.systemUrl = config.systemUrl;
+	data.categorySelected = '';
+	data.Moment = require('moment');
+
+
+	var url = req.originalUrl.split('/');
+	url = url.filter(function(n){ return n !== ''; });
+	data.url = url;
+	
+	if ( url.length >= 1 ) {
+		data.screen = url[0];
+		fs.exists('./views/'+data.screen+'.jade', function (exists) {
+			if (exists) {
+				fs.exists('./public/javascripts/'+data.screen+'.js', function (exists) {
+					data.script = (exists) ? '/javascripts/'+data.screen+'.js' : '';
+					data.subUrl = (url.length == 1 ) ? '' : url[1];
+					routes.index(req, res, data);
+				});
+			}
+			else {
+				routes.index(req, res, data);
+			}
+		});
+	}
+	else {
+		routes.index(req, res, data);
+	}
+});
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var request = require('request');
+exports.index = function(req, res, data){
+	if (data.screen == 'index') {
+		data.title = 'หน้าหลัก';
+	}
+
+	res.send('Hello World');
+
+	//exports.getCategory(req, res, data);
+
+};
+
+/*exports.getCategory = function(req, res, data){
+	try{
+		request.post({url: {data.apiUrl + '/category/info'},
+			form: {
+				apiKey: data.apiKey,
+				shop: data.shop
+			}
+		},
+		function (error, response, body) {
+			if (!error) {				
+				var json = JSON.parse(body);
+				data.category = json.result;
+				res.send(data);
+			} else{
+				data.error = error.message;
+				data.stack = error.stack;
+				res.render('error', { data: data });
+			}
+		});
+	}
+	catch(error) {
+		data.error = error.message;
+		data.stack = error.stack;
+		res.render('error', { data: data });
+	}
+};*/
